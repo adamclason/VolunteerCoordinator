283410bba31d8e49f035f9280b154cac653d5176
Adam Clason <adam.clason@covenant.edu>
2010-09-16 10:02:56 -0400

    Inital Commit

---
+++ src/com/VolunteerCoordinatorApp/VolunteerCoordinatorServlet.java
@@ -0,0 +1,13 @@
+package com.VolunteerCoordinatorApp;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class VolunteerCoordinatorServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
